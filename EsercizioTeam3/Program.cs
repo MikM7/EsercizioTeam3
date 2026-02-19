61279cd7dd187cc42ba31365605abbb5d9033228
Nambon03 <deciccodev@gmail.com>
2026-02-19 17:35:35 +0100

    Merge fatto con michele
    Update:
    + eseguito con successo il merge con la funzione di michele

--- EsercizioTeam3/Program.cs
+++ EsercizioTeam3/Program.cs
@@ -4,17 +4,7 @@ class Esercizio
 {
     public static void Main(string[] args)
     {
-    
-        static void StampaMenu(string[] gusti, double[] prezzi)
-        {
-            Console.WriteLine("--- Gelateria 2026**TEAM3** di Marco.Matteo.Michele ---");
-            Console.WriteLine("--- GUSTI DISPONIBILI ---");
-            for (int i = 0; i < gusti.Length; i++)
-            {
-                Console.WriteLine(i + ") " + gusti[i] + " - " + prezzi[i] + " Euro");
-            }
-            Console.WriteLine("-------------------------");
-        }
+        
     }
 
     public static float CalcolaTotale(int prezzoGusto, int quantita)
@@ -23,5 +13,16 @@ class Esercizio
 
         prezzoTotale = prezzoGusto * quantita;
         return prezzoTotale;
+    }
+
+    public static void StampaMenu(string[] gusti, double[] prezzi)
+    {
+        Console.WriteLine("--- Gelateria 2026**TEAM3** di Marco.Matteo.Michele ---");
+        Console.WriteLine("--- GUSTI DISPONIBILI ---");
+        for (int i = 0; i < gusti.Length; i++)
+        {
+            Console.WriteLine(i + ") " + gusti[i] + " - " + prezzi[i] + " Euro");
+        }
+        Console.WriteLine("-------------------------");
     }
 }
