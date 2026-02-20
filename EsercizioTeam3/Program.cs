c9b7223519fd4222d1fcd4e406a53cf0a2a57d70
Jomat22 <matteoborello0@gmail.com>
2026-02-20 10:31:08 +0100

    Squashed commit of the following:
    commit 60c55498d26c731a9535486c8f13004e5759cbaf
    Author: Nambon03 <deciccodev@gmail.com>
    Date:   Fri Feb 20 10:11:04 2026 +0100
    
        Sconto sistemato
    
        Fix:
        + sistemata logica sconto
    
    commit f070afd7bd0ff027ba2e1213139579e403287361
    Author: Nambon03 <deciccodev@gmail.com>
    Date:   Fri Feb 20 10:03:59 2026 +0100
    
        Modifiche sconto
    
        Update:
        + logica sconto modificato
    
    commit fb88adf504ac6eb39baade523312db1e9372c29c
    Author: Nambon03 <deciccodev@gmail.com>
    Date:   Thu Feb 19 18:32:04 2026 +0100
    
        Merge completo
    
        Update:
        + fatto merge completo
        + sistemato e collegato tutte le funzioni tra di loro
    
    commit d6624c4b7c223cb5741ae4768386eb070aca98d9
    Merge: 9d376fe aec347a
    Author: Nambon03 <deciccodev@gmail.com>
    Date:   Thu Feb 19 17:40:22 2026 +0100
    
        Merge branch 'BranchTeam3Matteo' into BranchTeam3Marco
    
    commit 9d376fefd3c74b4385d73868bfb2aa2aa6697730
    Author: Nambon03 <deciccodev@gmail.com>
    Date:   Thu Feb 19 17:38:46 2026 +0100
    
        Update Program.cs
    
        Update:
        + aggiunto commento alle funzioni
    
    commit 61279cd7dd187cc42ba31365605abbb5d9033228
    Author: Nambon03 <deciccodev@gmail.com>
    Date:   Thu Feb 19 17:35:35 2026 +0100
    
        Merge fatto con michele
    
        Update:
        + eseguito con successo il merge con la funzione di michele
    
    commit 188705a034a75979b46701191dc66873d140931b
    Merge: 6a94aa6 b1656d5
    Author: Nambon03 <deciccodev@gmail.com>
    Date:   Thu Feb 19 17:28:29 2026 +0100
    
        Merge branch 'BranchTeam3Michele' into BranchTeam3Marco
    
    commit b1656d57b7df676ce156553c64d565f9e7de8d81
    Author: MicheleM <michelepiomerla038@gmail.com>
    Date:   Thu Feb 19 17:21:00 2026 +0100
    
        gelateria
    
        stampa gusti GELATI
    
    commit 6a94aa6f6caa8d67a19732e80a10b57a4f96efea
    Author: Nambon03 <deciccodev@gmail.com>
    Date:   Thu Feb 19 17:19:39 2026 +0100
    
        Aggiunta funzione calcolo
    
        Update:
        + aggiunta funzione del calcolo prezzo

--- EsercizioTeam3/Program.cs
+++ EsercizioTeam3/Program.cs
@@ -4,13 +4,21 @@ class Esercizio
 {
     public static void Main(string[] args)
     {
-        double totale = 0;
+        float totale = 0, totaleScontato = 0;
         string riepilogo = "\n--- RIEPILOGO ORDINE ---\n";
         bool continua = true;
 
+        string[] gusti = { "Cioccolato", "Vaniglia", "Fragola", "Pistacchio", "Limone"};
+        float[] prezzi = { 2, 3, 4, 5, 2};
+        
+        int quantita;
+        string nomeGusto = "";
+        float prezzoPalla = 0;
+
         while (continua)
         {
-            StampaMenu();
+            
+            StampaMenu(gusti, prezzi);
 
             int scelta;
             Console.Write("Inserisci il numero del gusto (1-5): ");
@@ -20,28 +28,40 @@ class Esercizio
                 Console.WriteLine("Errore! Inserisci un numero valido tra 1 e 5:");
             }
 
-            int quantita;
             Console.Write("Quante palline vuoi di questo gusto? ");
             while (!int.TryParse(Console.ReadLine(), out quantita) || quantita <= 0)
             {
                 Console.WriteLine("Inserisci un numero valido (maggiore di 0):");
             }
 
-            string nomeGusto = "";
-            double prezzoPalla = 1.50;
-
             switch (scelta)
             {
-                case 1: nomeGusto = "Cioccolato"; break;
-                case 2: nomeGusto = "Vaniglia"; break;
-                case 3: nomeGusto = "Fragola"; break;
-                case 4: nomeGusto = "Pistacchio"; break;
-                case 5: nomeGusto = "Limone"; break;
+                case 1: 
+                    nomeGusto = gusti[scelta-1]; 
+                    prezzoPalla = prezzi[scelta-1];
+                    break;
+                case 2:
+                    nomeGusto = gusti[scelta-1]; 
+                    prezzoPalla = prezzi[scelta-1];
+                    break;
+                case 3:
+                    nomeGusto = gusti[scelta-1]; 
+                    prezzoPalla = prezzi[scelta-1];
+                    break;
+                case 4:
+                    nomeGusto = gusti[scelta-1]; 
+                    prezzoPalla = prezzi[scelta-1];
+                    break;
+                case 5:
+                    nomeGusto = gusti[scelta-1]; 
+                    prezzoPalla = prezzi[scelta-1];
+                    break;
             }
 
-            double costoParziale = prezzoPalla * quantita;
+            float costoParziale = CalcolaTotale(prezzoPalla, quantita);
             totale += costoParziale;
-            riepilogo += $"{quantita}x {nomeGusto} - {costoParziale:F2}€\n";
+            
+            riepilogo += $"Palline: {quantita}x - Gusto: {nomeGusto} - Costo: {costoParziale:F2}€\n";
 
             Console.WriteLine($"Aggiunto: {quantita} palline di {nomeGusto}.");
             
@@ -55,17 +75,34 @@ class Esercizio
     
         Console.WriteLine(riepilogo);
         Console.WriteLine("------------------------");
+
+        //if(totaleScontato > 0){Console.WriteLine($"TOTALE FINALE: {totaleScontato:F2}€ (SCONTATO)");}
         Console.WriteLine($"TOTALE FINALE: {totale:F2}€");
+        
         Console.WriteLine("Grazie e a presto!");
     }
-    public static void StampaMenu()
+
+    public static float CalcolaTotale(float prezzoGusto, int quantita)
     {
-        Console.WriteLine("\n--- GUSTI DISPONIBILI ---");
-        Console.WriteLine("1. Cioccolato");
-        Console.WriteLine("2. Vaniglia");
-        Console.WriteLine("3. Fragola");
-        Console.WriteLine("4. Pistacchio");
-        Console.WriteLine("5. Limone");
+        float prezzoTotale = 0;
+
+        prezzoTotale = prezzoGusto * quantita;
+
+        if(prezzoTotale > 10)
+        {
+            prezzoTotale *= 0.9f;
+        }
+        return prezzoTotale;       
+    }
+
+    public static void StampaMenu(string[] gusti, float[] prezzi)
+    {
+        Console.WriteLine("--- Gelateria 2026**TEAM3** di Marco.Matteo.Michele ---");
+        Console.WriteLine("--- GUSTI DISPONIBILI ---");
+        for (int i = 0; i < gusti.Length; i++)
+        {
+            Console.WriteLine(i+1 + ") " + gusti[i] + " - " + prezzi[i] + " Euro");
+        }
         Console.WriteLine("-------------------------");
     }
 }
